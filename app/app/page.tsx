573a26687af3bdd3d156de98bf3f2fe65c70168a
skyash101 <yashpc09@gmail.com>
2024-09-01 13:48:08 +0530

    merge conflicts

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import AppNavbar from "./components/AppNavbar";
 
 export default function Home() {
@@ -8,8 +7,4 @@ export default function Home() {
       <AppNavbar></AppNavbar>
     </main>
   );
-=======
-export default function Home() {
-  return <main className="">Hello Muzer!</main>;
->>>>>>> d5f15ca8fc97d5bae1dd4b8f1c4aebf5e04cfc1e
 }
